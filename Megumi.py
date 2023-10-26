e9b8564abed249929796c65cb12f57ef63a0cf7f
Pengu34 <noodlemegumi@gmail.com>
2023-10-25 20:20:18 -0400

    Hello

---
+++ Megumi.py
@@ -0,0 +1,27 @@
+"""
+To clone a repository:
+git clone (html page)
+
+when you want to do anything in the repository --> cs into the repository.
+
+to open the terminal:
+ctrl~
+
+to push to the repository:
+cd to the repository
+git stash
+git pull
+git add . 
+git commit -m "message" 
+git push
+
+
+to pull:
+git pull  <-- in GOSU
+
+to create a branch:
+git branch (name)
+
+
+
+"""
