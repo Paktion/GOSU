786a0f4ca1ce7de8b70f44f16da6fe6221d1491f
arlenf27 <feng.1301@buckeyemail.osu.edu>
2023-11-01 20:37:32 -0400

    Basic coordinates

--- backend.py
+++ backend.py
@@ -4,12 +4,14 @@ import json
 
 # importing googlemaps module
 import googlemaps
+
+gmaps = googlemaps.Client(key='AIzaSyBPc6V-4zZu_Xg7UGUoQVSImQX8-oN-C40')  
  
-# Requires API key
-gmaps = googlemaps.Client(key='AIzaSyBPc6V-4zZu_Xg7UGUoQVSImQX8-oN-C40')
- 
-# Requires cities name
-my_dist = gmaps.distance_matrix('Delhi','Mumbai')['rows'][0]['elements'][0]
- 
-# Printing the result
-print(my_dist)
+origin_latitude = 12.9551779
+origin_longitude = 77.6910334
+destination_latitude = 28.505278
+destination_longitude = 77.327774
+distance = gmaps.distance_matrix([str(origin_latitude) + " " + str(origin_longitude)], [str(destination_latitude) + " " + str(destination_longitude)], mode='walking')['rows'][0]['elements'][0]
+
+print(distance)
+
