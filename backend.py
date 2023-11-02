02f5983f825bfa6450720f933e606e17303649d7
alexhonigford <honigforda8@gmail.com>
2023-11-01 20:29:39 -0400

    a

--- backend.py
+++ backend.py
@@ -2,11 +2,14 @@ import googlemaps
 import requests
 import json
 
-#api key from google maps
-api_key = 'AIzaSyBPc6V-4zZu_Xg7UGUoQVSImQX8-oN-C40'
-
-source = input()
-
-dest = input()
-
-
+# importing googlemaps module
+import googlemaps
+ 
+# Requires API key
+gmaps = googlemaps.Client(key='AIzaSyBPc6V-4zZu_Xg7UGUoQVSImQX8-oN-C40')
+ 
+# Requires cities name
+my_dist = gmaps.distance_matrix('Delhi','Mumbai')['rows'][0]['elements'][0]
+ 
+# Printing the result
+print(my_dist)
