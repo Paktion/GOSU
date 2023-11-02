f1fe5b8c1f21fdeb4bf202f323668feb93226634
alexhonigford <honigforda8@gmail.com>
2023-11-01 20:39:29 -0400

    s

--- backend.py
+++ backend.py
@@ -6,11 +6,13 @@ import json
 import googlemaps
 
 gmaps = googlemaps.Client(key='AIzaSyBPc6V-4zZu_Xg7UGUoQVSImQX8-oN-C40')  
- 
+
+#source and destination coordinate pairs being stores 
 origin_latitude = 12.9551779
 origin_longitude = 77.6910334
 destination_latitude = 28.505278
 destination_longitude = 77.327774
+#uses the distance_matrix function to get the distance between the coordinate pairs
 distance = gmaps.distance_matrix([str(origin_latitude) + " " + str(origin_longitude)], [str(destination_latitude) + " " + str(destination_longitude)], mode='walking')['rows'][0]['elements'][0]
 
 print(distance)
